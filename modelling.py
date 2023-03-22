9a47265f2b532b34901cfa47b7fe225e7ef3562b
Amalaseeli <amalremi07@gmail.com>
2023-03-22 15:03:07 +0000

    Fit model

--- modelling.py
+++ modelling.py
@@ -1,32 +1,49 @@
-from tabular_data import clean_tabular_data 
+from tabular_data import clean_tabular_data
 import pandas as pd
+import numpy as np
 # from sklearn import datasets, model_selection
 from sklearn.linear_model import SGDRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-tabular_data=clean_tabular_data()
-
-df=pd.read_csv('./tabular_data/listing.csv')
-kwargs={'label':'Price_Night', 'features':['beds', 'bathrooms','Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','amenities_count','Unnamed: 19']}
-
-X,y=tabular_data.load_airbnb(df,**kwargs)
-print(X,y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(len(X_train))
-print(len(X_test))
-print(len(y_train))
-print(len(y_test))
-reg = make_pipeline(StandardScaler(),
-                    SGDRegressor(max_iter=1000, tol=1e-3))
-
-reg.fit(X_train, y_train)
-
-# y_pred = reg.predict(X_test)
-# mse = mean_squared_error(y_test, y_pred)
-# print("Mean squared error:", mse)
 
 
 
+def split_data(X, y):
+    '''Splits Test, Train and Validation data'''
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5)
+    return X_train, y_train, X_test, y_test, X_validation, y_validation
+
+if __name__ == '__main__':
+
+    # load the data
+    tabular_data=clean_tabular_data()
+    df = pd.read_csv('clean_data.csv')
+    X,y=tabular_data.load_airbnb(df)
+    X=X.to_numpy()
+    y=y.to_numpy()
+    print(type(X))
+    print(X.shape)
+    print(y.shape)
+
+   
+    #split data in train, test and validation sets
+    X_train, y_train, X_test, y_test, X_validation, y_validation = split_data(X,y)
+    print(len(X_train))
+    print(len(X_test))
+    print(len(y_train))
+    print(len(y_test))
+
+    # # select model
+    model = SGDRegressor()
+    model.fit(X_train, y_train)
+
+
+    y_train_pred = model.predict(X_train)
+    # y_validation_pred = model.predict(X_validation)
+    # y_test_pred = model.predict(X_test)
+    
 
--- tabular_data.py
+++ tabular_data.py
@@ -1,77 +1,38 @@
 import pandas as pd
-from ast import literal_eval
-from pandas.api.types import is_numeric_dtype
 
 class clean_tabular_data():
     def __init__(self):
         self.df=pd.read_csv('./tabular_data/listing.csv')
 
     def remove_rows_with_missing_ratings(self):
-        df2=self.df.dropna(subset=["Cleanliness_rating",'Accuracy_rating', 'Communication_rating'	,'Location_rating',	'Check-in_rating' ,	'Value_rating' ])
-        print(df2)
-        print(type(df2))
-    
-    # def literal_return(val):
-    #         try:
-    #             return literal_eval(val)
-    #         except (ValueError, SyntaxError) as e:
-    #             return val 
-
-    # def combine_description_strings(df):
-    #     df['Description']=df['Description'].apply(lambda x: literal_return(x))
-    #     df.Description=df.Description.str.join(",")
-    #     return df
-
+        self.df=self.df.dropna(subset=["Cleanliness_rating",'Accuracy_rating', 'Communication_rating'	,'Location_rating',	'Check-in_rating' ,	'Value_rating' ])
+       
     def combine_description_strings(self):
-        df = self.df.dropna(subset=['Description'])
-        df["Description"] = df["Description"].apply(lambda x: x.replace("'About this space', ", '').replace("'', ", '').replace('[', '').replace(']', '').replace('\\n', '.      ').replace("''", '').split(" "))
-        df["Description"] = df["Description"].apply(lambda x: " ".join(x))
-        return df
-
-    def set_default_feature_values(self, df):
-        df[["guests", "beds", "bathrooms","bedrooms"]]=df[["guests", "beds", "bathrooms","bedrooms"]].fillna(value=1)
-        return df
-
-    def load_airbnb(self,df,**kwargs):
-        for col in df.columns:
-            if is_numeric_dtype(df[col])== False:
-                df=df.drop(col, axis=1)
-        # print(df.head())
-        # for col in df.columns:
-        #     print(col)
-        label=df[kwargs['label']]
-        features=df[kwargs['features']]
-        # print('label\n',label)
-        # print('features\n',features)
-        return label,features
+        self.df = self.df.dropna(subset=['Description'])
+        self.df["Description"] = self.df["Description"].apply(lambda x: x.replace("'About this space', ", '').replace("'', ", '').replace('[', '').replace(']', '').replace('\\n', '.      ').replace("''", '').split(" "))
+        self.df["Description"] = self.df["Description"].apply(lambda x: " ".join(x))
         
+    def set_default_feature_values(self):
+        self.df[["guests", "beds", "bathrooms","bedrooms"]]=self.df[["guests", "beds", "bathrooms","bedrooms"]].fillna(value=1)
 
+    def load_airbnb(self,df):
+        df = df.drop(columns=["Unnamed: 19"])
+        df = df.drop(columns=["Unnamed: 0"])
 
+        features = df.drop(columns=['Price_Night', 'ID', 'Category', 'Title', 'Description', 'Amenities', 'Location', 'url'])
+        labels = df["Price_Night"]
 
-
-        # features=df.drop(df[kwargs],axis=1)
-        # label={}
-        
-        # print(features,label)
-        # return features,label
-
-        
-        # numeric=df._get_numeric_data()
-        #
-        # features=df.drop(df[label], axis=1)
-        
-        # print(numeric)
-        # print(features,label)
-        # return features,label
+        features_labels  = (features, labels)
+        return features_labels    
     
 if __name__ =='__main__':
     tabular_data=clean_tabular_data()
     tabular_data.remove_rows_with_missing_ratings()
-    df2=tabular_data.combine_description_strings()
-    print(df2['Description'])
-    df3=tabular_data.set_default_feature_values(df2)
-    print(df3)
-    df3.to_csv('clean_data.csv')
-    
-    kwargs={'label':'Price_Night', 'features':['beds', 'bathrooms','Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating','amenities_count','Unnamed: 19']}
-    tabular_data.load_airbnb(df3,**kwargs)
+    tabular_data.combine_description_strings()
+    tabular_data.set_default_feature_values()
+    tabular_data.df.to_csv('clean_data.csv')
+    df = pd.read_csv('clean_data.csv')
+    feature_labels = tabular_data.load_airbnb(df)
+    feature, labels = feature_labels
+    print(feature)
+    print(labels)
