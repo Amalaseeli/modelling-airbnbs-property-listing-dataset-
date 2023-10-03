169007b8a302be1206c347232fb11dcff1b1ddb4
Amalaseeli <amalremi07@gmail.com>
2023-10-03 16:54:27 +0100

    Classification model

--- modelling_classification.py
+++ modelling_classification.py
@@ -8,6 +8,7 @@ import numpy as np
 import hyperparameter
 from sklearn.linear_model import LogisticRegression
 import sklearn
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,confusion_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -40,6 +41,65 @@ def get_metrics_for_classification_model(y_train, y_train_pred, y_test, y_test_p
 
     return performance_metric
 
+def tune_classification_model_hyperparameters(model, X_train, y_train, X_test, y_test, X_validation, y_validation, parameter_grid:dict):
+    grid_search =GridSearchCV(
+        estimator = model,
+        param_grid = parameter_grid
+    )
+
+    grid_search = grid_search.fit(X_train, y_train)
+
+    # finding the best model and hyperparameters from grid_search
+    best_model = grid_search.best_estimator_
+    best_params = grid_search.best_params_
+
+    y_train_pred, y_test_pred, y_validation_pred = make_predictions(best_model, X_train, X_test, X_validation)
+    performance_metric = get_metrics_for_classification_model(y_train, y_train_pred, y_test, y_test_pred, y_validation, y_validation_pred)
+
+    return best_model, best_params, performance_metric
+
+def evaluate_all_cls_models(model_list, parameter_grid_list):
+
+    # looping over models and corresponding parameter grids
+    for i in range(len(model_list)):
+
+        model = model_list[i]               
+        parameter_grid = parameter_grid_list[i]
+
+        # finds the best model, parameters and performance metrics
+        best_model, best_params, performance_metric = tune_classification_model_hyperparameters(model, X_train, y_train, X_test, y_test, X_validation, y_validation, parameter_grid)
+        
+        # defines folder name to save the model, parameter and metrics
+        model_name = str(model)[:-2]        
+        folder_path = f"models/classification/{model_name}"
+
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Saves the model, parameter and metrics in a folder
+        hyperparameter.save_model(folder_path, best_model, best_params, performance_metric)
+        print(f"Best model, parameters and metrics saved for {model}")
+        print("--" * 10)
+
+def find_best_cls_model(model_list,folder):
+    dict={}
+    for model in model_list:
+        model_name=str(model)[:-2]
+        metric_files=f"{folder}/{model_name}/metrics.json"
+
+        with open(metric_files) as json_file:
+            metric=json.load(json_file)
+        dict[model_name] = metric['accuracy_validation']
+    best_model_name = min(dict, key=dict.get)
+    with open(f"{folder}/{best_model_name}/hyperparameters.json") as json_file:
+            parameters = json.load(json_file)
+
+    with open(f"{folder}/{best_model_name}/metrics.json") as json_file:
+            performance_metric = json.load(json_file)
+
+    best_reg_model = joblib.load(f"{folder}/{best_model_name}/model.joblib")
+
+    return best_reg_model, parameters, performance_metric
+
 if __name__=='__main__':
     file='clean_tabular_data.csv'
     df=pd.read_csv(file)
@@ -54,6 +114,62 @@ if __name__=='__main__':
 
     # estimate predicted labels
     y_train_pred, y_test_pred, y_validation_pred = make_predictions(model, X_train, X_test, X_validation)
-    performance_metric=get_metrics_for_classification_model(y_train,y_train_pred,y_test,y_test_pred,y_validation,y_validation_pred)
-    
 
+    #Get performance metrices for classification model
+    performance_metric=get_metrics_for_classification_model(y_train,y_train_pred,y_test,y_test_pred,y_validation,y_validation_pred)
+
+    #printing the metrices
+    print(f"\t Train \t Test \t Validation")
+    print(f" Accuracy \t {performance_metric['accuracy_train']:.2f} \t {performance_metric['accuracy_test']:.2f}  \t {performance_metric['accuracy_validation']:.2f}")
+    print(f" Precision \t {performance_metric['precision_train']:.2f} \t {performance_metric['precision_test']:.2f}  \t {performance_metric['precision_validation']:.2f}")
+    print(f" Recall \t {performance_metric['recall_train']:.2f} \t {performance_metric['recall_test']:.2f}  \t {performance_metric['recall_validation']:.2f}")
+    print(f" F1  \t\t {performance_metric['f1_train']:.2f} \t {performance_metric['f1_test']:.2f}  \t {performance_metric['f1_validation']:.2f}")
+
+    parameter_grid={"C":np.logspace(-3,3,7), "penalty":["l2"]}
+
+    best_model, best_params, performance_metric = tune_classification_model_hyperparameters(model, X_train, y_train, X_test, y_test, X_validation, y_validation, parameter_grid)
+    folder_path = "models/classification/logistic_regression"
+
+    os.makedirs(folder_path, exist_ok=True)
+
+    # Saves the tuned model, parameter and metrics in a folder
+    hyperparameter.save_model(folder_path, best_model, best_params, performance_metric)
+
+    #Now tuning three other models: decision trees, random forests, and gradient boosting
+
+    # DecisionTreeClassifier
+    parameter_grid_dtc={
+        "criterion": ['gini', 'entropy'],
+        "max_depth": range(1,10),
+        "min_samples_split": range(1,10),
+        "min_samples_leaf": range(1,5)
+    }
+    
+    # RandomForestClassifier
+    parameter_grid_rfc = { 
+        'n_estimators': [200, 500],
+        'max_depth' : [4,5,6],
+        'criterion' :['gini', 'entropy']
+    }
+
+    #GradientBoostingClassifier
+    parameter_grid_gbc = {
+        "n_estimators": [25, 50],
+        "loss": ["log_loss"]
+    }
+
+    model_list = [
+        DecisionTreeClassifier(),
+        RandomForestClassifier(),
+        GradientBoostingClassifier()
+    ]
+
+    parameter_grid_list= [parameter_grid_dtc, parameter_grid_rfc, parameter_grid_gbc]
+
+    evaluate_all_cls_models(model_list, parameter_grid_list)
+    
+    folder = "models/classification"
+    best_cls_model, parameters, performance_metric = find_best_cls_model(model_list, folder)
+
+    print(f"The best classification model is {best_cls_model}")
+    print(f"Performance metric for the best model is {performance_metric}")
